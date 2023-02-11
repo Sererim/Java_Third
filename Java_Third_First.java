267d09763b6943e83e01614e228a710d2476a30b
Sererim <danielleymanw@gmail.com>
2023-02-11 18:54:09 +0300

    Working on getting input from a user.

--- Java_Third_First.java
+++ Java_Third_First.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Java_Third_First{
-    
+
+    public static ArrayList <Integer> get_nums() {
+        Scanner scan = new Scanner(System.in);
+        ArrayList <Integer> nums = new ArrayList<>(); 
+        while(true){        
+            try {
+                System.out.println("Enter integers.");
+                nums.add(scan.nextInt());
+            }
+            catch(Exception InputMismatchException)
+            {
+                System.out.println("Not an integer was entered!");
+            }
+            break;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Java_Third_First.get_nums();
+    }
 }

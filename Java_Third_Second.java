dca1719a03bc1dc9e9aa92cc03606923fe8eef62
Sererim <danielleymanw@gmail.com>
2023-02-12 17:13:21 +0300

    Defined all needed functions and class variables.

--- Java_Third_Second.java
+++ Java_Third_Second.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.undo.StateEdit;
+
 public class Java_Third_Second{
     
+    private static ArrayList <Integer> nums = new ArrayList<>();
+    private static Scanner scan = new Scanner(System.in);
+    private static int size = 0;
+
+    private static void get_nums()
+    {
+
+    }
+
+    private static void get_min_max()
+    {
+
+    }
+
+    private static void get_mean_num()
+    {
+
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }

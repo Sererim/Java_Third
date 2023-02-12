d2d3b6dd5f22ba3961e9827ef3ae01fcf0684107
Sererim <danielleymanw@gmail.com>
2023-02-12 17:27:05 +0300

    Second exercise is finished.

--- Java_Third_Second.java
+++ Java_Third_Second.java
@@ -1,7 +1,6 @@
+import java.text.Format;
 import java.util.ArrayList;
 import java.util.Scanner;
-
-import javax.swing.undo.StateEdit;
 
 public class Java_Third_Second{
     
@@ -9,22 +8,66 @@ public class Java_Third_Second{
     private static Scanner scan = new Scanner(System.in);
     private static int size = 0;
 
-    private static void get_nums()
-    {
-
+    public static void get_nums() {
+        System.out.println("Enter the lenght of an array of integers.");
+        try{
+            size = scan.nextInt();
+        }
+        catch (Exception InputMismatchException)
+        {
+            System.out.println("Entered not a number of size is to large.");
+            Java_Third_Second.get_nums();
+        }
+        System.out.println("Enter integers");       
+            for(int i = 0; i < size; i++)
+            {
+                try{
+                    nums.add(scan.nextInt());
+                }
+                catch (Exception InputMismatchException)
+                {
+                    System.out.println("Not an integer was entered!");
+                }
+            }
     }
 
-    private static void get_min_max()
+    private static String get_min_max()
     {
+        int[] min_max = {nums.get(0), nums.get(0)}; // {min , max}
 
+        for(int i = 0; i < size; i++)
+        {
+            if(min_max[0] > nums.get(i))
+                min_max[0] = nums.get(i);
+            if(min_max[1] < nums.get(i))
+                min_max[1] = nums.get(i);
+        }
+        String bar = "min: " + min_max[0] + " max: " + min_max[1];
+        return bar;
     }
 
-    private static void get_mean_num()
+    private static int get_mean_num()
     {
-
+        int foo = 0;
+        for(int i = 0; i < size; i++)
+            foo += nums.get(i);
+        return (foo / size);
     }
-    
+
     public static void main(String[] args) {
-        
+        String foo = "";
+        while(true)
+        {
+            Java_Third_Second.get_nums();
+            System.out.println(nums);
+            System.out.println(Java_Third_Second.get_min_max());
+            System.out.println(Java_Third_Second.get_mean_num());
+
+            System.out.println("Do you want to terminate the program y/Y ?");
+            foo = scan.next();
+            if(foo.equals("y") || foo.equals("Y"))
+                break;
+        }
+        scan.close();
     }
 }
